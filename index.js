95bcbced681265703253e835f4ae6a3e60feea10
Mario <maalnu93@hotmail.com>
2023-11-08 08:51:14 +0100

    Primeros pasos

---
+++ index.js
@@ -0,0 +1,7 @@
+const nombre = prompt("INTRODUCE TU NOMBRE")
+const apellidos = prompt("INTRODUCE TUS APELLIDOS")
+const fechaN = prompt("INTRODUCE TU FECHA DE NACIMIENTO")
+
+const h1 = document.createElement("h1")
+h1.innerText = "Práctica 3 - DWEC"
+document.body.appendChild(h1)
