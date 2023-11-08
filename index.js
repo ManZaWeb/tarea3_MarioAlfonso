981340f24f093a27b9b8269794b998f746df86ff
Mario <maalnu93@hotmail.com>
2023-11-08 09:48:52 +0100

    Primeros pasos 3

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-const nombre = prompt("INTRODUCE TU NOMBRE")
-const apellidos = prompt("INTRODUCE TUS APELLIDOS")
-const fechaN = prompt("INTRODUCE TU FECHA DE NACIMIENTO")
-nombreCompleto = nombre.concat(apellidos)
+document.addEventListener("DOMContentLoaded", () => {
 
-const h1 = document.createElement("h1")
-h1.innerText = "Práctica 3 - DWEC"
-document.body.appendChild(h1)
+    const solicitarNombre = () => {
+        let nombre;
+        do {
+            nombre = prompt("INTRODUCE TU NOMBRE");
+        } while (nombre === "");
+        return nombre; // Devolver el nombre ingresado
+    }
 
-const saludo = document.createElement("div")
-saludo.innerText = `Buenos días ${nombre}`
-document.body.appendChild(saludo)
+    const solicitarApellidos = () => {
+        let apellidos;
+        do {
+            apellidos = prompt("INTRODUCE TUS APELLIDOS");
+        } while (apellidos === "");
+        return apellidos; // Devolver los apellidos ingresados
+    }
 
-const longitud = document.createElement("div")
-longitud.innerText = `La longitud de su nombre completo es ${nombreCompleto.length}`
-document.body.appendChild(longitud)
+    const solicitarFechaN = () => {
+        let fechaN;
+        do {
+            fechaN = prompt("INTRODUCE TU FECHA DE NACIMIENTO");
+        } while (fechaN === "");
+        return fechaN; // Devolver la fecha de nacimiento ingresada
+    }
+
+    const nombre = solicitarNombre();
+    const apellidos = solicitarApellidos();
+    const fechaN = solicitarFechaN();
+
+    const nombreCompleto = `${nombre} ${apellidos}`;
+
+    const h1 = document.createElement("h1")
+    h1.innerText = "Práctica 3 - DWEC"
+    document.body.appendChild(h1)
+
+    const saludo = document.createElement("div")
+    saludo.innerText = `Buenos días ${nombre}`
+    document.body.appendChild(saludo)
+
+    const longitud = document.createElement("div")
+    longitud.innerText = `La longitud de su nombre completo es ${nombreCompleto.length}`
+    document.body.appendChild(longitud)
+
+
+    const primeraA = document.createElement("div")
+    primeraA.innerText = `La primera letra A de tu nombre está en la posición ${nombreCompleto.indexOf('A')}`
+    document.body.appendChild(primeraA)
+    //funciona pero si no encuentra nada resultado -1
+})
